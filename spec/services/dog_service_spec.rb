1eb7a7e2c4ace2762dfb7086884bccafc9b38e7f
mmbensalah <mmbensalah@gmail.com>
2019-02-23 19:19:36 -0700

    Remove VCR from test

--- spec/services/dog_service_spec.rb
+++ spec/services/dog_service_spec.rb
@@ -1,13 +1,9 @@
 require 'rails_helper'
 describe DogService do
-  context 'some context' do
-  VCR.use_cassette('any') do
-    it 'returns dog info in json' do
-      service = DogService.new
-      result = service.get_poodles
-      expect(result[:status]).to eq('success')
-      expect(result[:message].class).to eq(Array)
-    end
+  it 'returns dog info in json' do
+    service = DogService.new
+    result = service.get_poodles
+    expect(result[:status]).to eq('success')
+    expect(result[:message].class).to eq(Array)
   end
-end
 end
