41744d6bf5fa4f4d02d026ddf73a80d9fcd2ad37
mmbensalah <mmbensalah@gmail.com>
2019-02-26 21:31:55 -0700

    Return create poodles method to welcome controller - will try and move in next refactor

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    create_poodle_objects
     @images = Poodle.all
+  end
+
+  def create_poodle_objects
+    service = DogService.new
+    service.create_poodles
   end
 end
--- app/models/poodle.rb
+++ app/models/poodle.rb
@@ -1,6 +1,3 @@
 class Poodle < ApplicationRecord
   
-  def create_poodle_objects
-    DogService.new.create_poodles
-  end
 end
