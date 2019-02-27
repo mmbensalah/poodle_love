6ca56347a1d889cc3262a57471a23cd0bd5c3a51
mmbensalah <mmbensalah@gmail.com>
2019-02-26 21:26:38 -0700

    Redirect PATCH to go to herokuapp not local host

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
     var id      = this.id;
     $.ajax({
       type: 'PATCH',
-      url: "/api/v1/poodles",
+      url: "https://poodle-love.herokuapp.com/api/v1/poodles",
       data: { "id": id, "ranking": ranking },
       success: function(result) {
         alert('Poodle has been ranked!');
@@ -34,31 +34,31 @@ $(document).ready(function(){
       }
     });
   })
-  $(".sort").on('click', function() {
-    $.ajax({
-      type: 'GET',
-      url: "/api/v1/sort",
-      success: function(result) {
-        $( ".images" ).empty();
-        result.forEach(function(poodle) {
-          $( ".images" ).append(
-          `<div class="image"><img src=${poodle.url} alt="dog">
-            <select class="dropdown">
-              <option value="0">Select Ranking</option>
-              <option value="1">Best</option>
-              <option value="2">Great</option>
-              <option value="3">Good</option>
-              <option value="4">Cute</option>
-              <option value="5">Shoo</option>
-            </select>
-            <button class="btn-patch" id="<%= image.id %>">Rank!</button>
-          </div>`)
-        })
-        alert('Sorted');
-      },
-      error: function(response) {
-        alert(response.responseJSON.error);
-      }
-    });
-  })
+  // $(".sort").on('click', function() {
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: "/api/v1/sort",
+  //     success: function(result) {
+  //       $( ".images" ).empty();
+  //       result.forEach(function(poodle) {
+  //         $( ".images" ).append(
+  //         `<div class="image"><img src=${poodle.url} alt="dog">
+  //           <select class="dropdown">
+  //             <option value="0">Select Ranking</option>
+  //             <option value="1">Best</option>
+  //             <option value="2">Great</option>
+  //             <option value="3">Good</option>
+  //             <option value="4">Cute</option>
+  //             <option value="5">Shoo</option>
+  //           </select>
+  //           <button class="btn-patch" id="<%= image.id %>">Rank!</button>
+  //         </div>`)
+  //       })
+  //       alert('Sorted');
+  //     },
+  //     error: function(response) {
+  //       alert(response.responseJSON.error);
+  //     }
+  //   });
+  // })
 });
